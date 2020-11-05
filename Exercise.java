1da4cf9572c1585ecac3a343e3e04dc850707af4
museofkeys3187 <rwenzlick@ltu.edu>
2020-11-04 22:47:34 -0500

    Update Exercise.java
    implemented method to get graphics and tested with a test graphic

--- Exercise.java
+++ Exercise.java
@@ -1,4 +1,24 @@
-
-public class Exercise {
-
+import java.awt.*;
+import javax.swing.*;
+import java.applet.*;
+public class Exercise extends JFrame{
+	private ImageIcon image1;
+	private JLabel label1;
+	
+	//create a constructor to get the image
+	Exercise(){
+		setLayout(new FlowLayout());
+		
+		image1 = new ImageIcon(getClass().getResource("nah.jpeg"));
+		
+		label1 = new JLabel(image1);
+		add(label1);
+	}
+	public static void main(String[] args){
+		Exercise gui = new Exercise();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
+		gui.pack();
+		gui.setTitle("Daily Exercise Recommendation");
+	}
 }
